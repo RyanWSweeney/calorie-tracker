558cd1dbcb2e05c0640022fbda7486783753f6ee
Ryan Sweeney <107775698+RyanWSweeney@users.noreply.github.com>
2023-06-02 20:10:47 -0500

    Added todo list with current plans for TODO

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -44,7 +44,7 @@ function ResponsiveAppBar() {
                         variant="h6"
                         noWrap
                         component="a"
-                        href="/"
+                        href="/dashboard"
                         sx={{
                             mr: 2,
                             display: { xs: 'none', md: 'flex' },
